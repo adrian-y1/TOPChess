6f3ad7677955792c9e4a5e0ad468bd6c3096cfc2
adrian-y1 <adrian.yonan1@gmail.com>
2022-12-25 15:54:50 +1100

    included all dependencies inside main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+require_relative './game_pieces/bishop'
+require_relative './game_pieces/king'
+require_relative './game_pieces/knight'
+require_relative './game_pieces/pawn'
+require_relative './game_pieces/queen'
+require_relative './game_pieces/rook'
+require_relative './modules/game_info'
+require_relative './modules/save_load'
+require_relative './modules/validate_moves'
+require_relative './board_setup'
 require_relative './board'
 require_relative './end_game_manager'
+require_relative './player'
 require_relative './game'
 require 'colorize'
 
